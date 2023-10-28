4d4045cd52aa2ac3a955d0224d54d0c5bf8a75a8
Hasitha Edirisinghe <hasithale@gmail.com>
2023-10-29 04:14:48 +0530

    development: created all operations in main file

--- Program.cs
+++ Program.cs
@@ -1,44 +1,87 @@
 ﻿using Inventory_Management_System;
 
-Console.WriteLine("*** Inventory Managements System***\n");
-Console.WriteLine("1. Add a new product\n2. Remove a product\n3. Update the quantity or price\n4. View the list of all products\n");
-Console.Write("Enter the Number: ");
-int input = Convert.ToInt32(Console.ReadLine());
 List<Product> productList = new List<Product>();
+Console.WriteLine("*** Inventory Managements System***\n");
 
-switch (input)
+while (true)
 {
-	case 1:
-		Console.WriteLine("Case 1");
-		Console.Write("Enter Product ID: ");
-		string productID = Console.ReadLine();
-		Console.Write("Enter Product Name: ");
-		string productName = Console.ReadLine();
-		Console.Write("Enter Quantity: ");
-		int productQty = Convert.ToInt32(Console.ReadLine());
-		Console.Write("Enter Price per unit: ");
-		double productPricePU = Convert.ToDouble(Console.ReadLine());
+	Console.WriteLine("1. Add a new product\n2. Remove a product\n3. Update the quantity or price\n4. View the list of all products\n");
+	Console.Write("Enter the Number: ");
+	int input = Convert.ToInt32(Console.ReadLine());
 
-		var product = new Product(productID,productName,productQty,productPricePU);
-		productList.Add(product);
-		Console.WriteLine("New Product Added\n");
-        foreach (var item in productList)
-        {
-			Console.WriteLine($"Product ID: {item.productID}\nProduct Name: {item.productName}\nQty: {item.productQty}\nPrice: {item.pricePerUnit}");
-        }
-        Console.WriteLine("\nEnd");
-		break;
-	case 2:
-		Console.WriteLine("Case 2");
-		break;
-	case 3:
-		Console.WriteLine("Case 3");
-		break;
-	case 4:
-		Console.WriteLine("Case 4");
-		break;
-	default:
-		Console.WriteLine("Default Case");
-		break;
+	switch (input)
+	{
+		case 1:
+			Console.WriteLine("Case 1");
+			Console.Write("Enter Product ID: ");
+			string productID = Console.ReadLine();
+			Console.Write("Enter Product Name: ");
+			string productName = Console.ReadLine();
+			Console.Write("Enter Quantity: ");
+			int productQty = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter Price per unit: ");
+			double productPricePU = Convert.ToDouble(Console.ReadLine());
+
+			var product = new Product(productID, productName, productQty, productPricePU);
+			productList.Add(product);
+			Console.WriteLine("New Product Added\n");
+			Console.WriteLine($"Product ID: {productID}\nProduct Name: {productName}\nQty: {productQty}\nPrice: {productPricePU}\n");
+			Console.WriteLine("\nEnd");
+			break;
+		case 2:
+			Console.WriteLine("Case 2");
+			Console.Write("Enter Product ID you want to delete: ");
+			string deleteID = Console.ReadLine();
+			foreach (var item in productList.ToList())
+			{
+				if (item.productID == deleteID)
+				{
+					productList.Remove(item);
+					Console.WriteLine("\nProduct Deleted");
+				}
+			}
+			Console.WriteLine("\nEnd");
+			break;
+		case 3:
+			Console.WriteLine("Case 3");
+			Console.Write("Enter Product ID you want to Update: ");
+			string updateID = Console.ReadLine();
+			foreach (var item in productList.ToList())
+			{
+				if (item.productID == updateID)
+				{
+					Console.WriteLine($"Product ID: {item.productID}\nProduct Name: {item.productName}\nQty: {item.productQty}\nPrice: {item.pricePerUnit}\n");
+					Console.WriteLine("1-Update quantity\n2-Update Price");
+					Console.Write("what you want update? ");
+					string whatUpdate = Console.ReadLine();
+					if (whatUpdate == "1")
+					{
+						Console.Write("Enter New Quntity: ");
+						int newQuantity = Convert.ToInt32(Console.ReadLine());
+						item.productQty = newQuantity;
+						Console.WriteLine("\nProduct Quantity Updated");
+					}
+					else if (whatUpdate == "2")
+					{
+						Console.Write("Enter New Price: ");
+						double newPrice = Convert.ToDouble(Console.ReadLine());
+						item.pricePerUnit = newPrice;
+						Console.WriteLine("\nProduct Price Updated");
+					}
+				}
+			}
+			Console.WriteLine("\nEnd");
+			break;
+		case 4:
+			Console.WriteLine("Case 4");
+			foreach (var item in productList)
+			{
+				Console.WriteLine($"Product ID: {item.productID}\nProduct Name: {item.productName}\nQty: {item.productQty}\nPrice: {item.pricePerUnit}\n");
+			}
+			Console.WriteLine("\nEnd");
+			break;
+		default:
+			Console.WriteLine("Default Case");
+			break;
+	}
 }
-
